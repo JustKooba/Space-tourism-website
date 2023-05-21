59b71d34d5786cbfe92966fdb2e2e43ff350ed7f
JustKooba <jakubsakw2009@gmail.com>
2023-05-21 22:09:04 +0200

    changed site layout

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,8 +1,16 @@
 <template>
-  00 Home 01 Destination 02 Crew 03 Technology So, you want to travel to Space
-  Let’s face it; if you want to go to space, you might as well genuinely go to
-  outer space and not hover kind of on the edge of it. Well sit back, and relax
-  because we’ll give you a truly out of this world experience! Explore
+  <div class="content">
+    <div class="menu">00 Home 01 Destination 02 Crew 03 Technology</div>
+
+    <h2 class="home-subtitle">So, you want to travel to</h2>
+    <h1 class="home-title">Space</h1>
+    <p class="home-desc">
+      Let’s face it; if you want to go to space, you might as well genuinely go
+      to outer space and not hover kind of on the edge of it. Well sit back, and
+      relax because we’ll give you a truly out of this world experience!
+    </p>
+    <button class="explore">Explore</button>
+  </div>
 </template>
 
 <script>
@@ -32,5 +40,34 @@ body {
     min-width: 100vw;
     background-image: url("../assets/home/background-home-desktop.jpg");
   }
+}
+
+.home-subtitle,
+.home-desc {
+  color: #8d8f9e;
+  text-align: center;
+}
+
+.home-title {
+  color: #fff;
+  text-align: center;
+}
+
+.explore {
+  border-radius: 50%;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 130px;
+  width: 130px;
+  font-size: 30px;
+}
+
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
