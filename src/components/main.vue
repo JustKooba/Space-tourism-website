c758d2b3bd7c8ece96e8bbc8966034389f81fb42
JustKooba <jakubsakw2009@gmail.com>
2023-05-22 20:44:55 +0200

    styled the home page, added new fonts

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="content">
-    <div class="menu">00 Home 01 Destination 02 Crew 03 Technology</div>
+  <div class="menu">
+    <img src="../assets/shared/icon-hamburger.svg" alt="three lines, menu" />
 
+    <div class="menu-content">00 Home 01 Destination 02 Crew 03 Technology</div>
+  </div>
+  <div class="content">
     <h2 class="home-subtitle">So, you want to travel to</h2>
     <h1 class="home-title">Space</h1>
     <p class="home-desc">
@@ -18,6 +21,12 @@ export default {};
 </script>
 
 <style>
+@font-face {
+  font-family: majesty-display;
+  src: url("../assets/fonts/Majesty Bold.ttf");
+}
+
+@import url("https://fonts.googleapis.com/css2?family=Lato:wght@100;300&display=swap");
 body {
   background-image: url("../assets/home/background-home-tablet.jpg");
   -webkit-background-size: cover;
@@ -44,13 +53,32 @@ body {
 
 .home-subtitle,
 .home-desc {
-  color: #8d8f9e;
+  color: #a9aab8;
   text-align: center;
+  font-family: "lato", sans-serif;
+  line-height: 30px;
+}
+.home-desc {
+  font-size: 20px;
+}
+.home-subtitle {
+  text-transform: uppercase;
+  text-align: left;
+  margin-bottom: 15px;
+  font-size: 22px;
+  font-weight: 100;
+  margin-right: 20px;
 }
 
 .home-title {
   color: #fff;
   text-align: center;
+  text-transform: uppercase;
+  font-family: "majesty-display", sans-serif;
+  font-size: 100px;
+  letter-spacing: 25px;
+  margin-bottom: 20px;
+  margin-top: 15px;
 }
 
 .explore {
@@ -59,9 +87,13 @@ body {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 130px;
-  width: 130px;
+  height: 170px;
+  width: 170px;
   font-size: 30px;
+  text-transform: uppercase;
+  font-family: "lato", sans-serif;
+  font-weight: 400;
+  margin-top: 20vh;
 }
 
 .content {
@@ -69,5 +101,10 @@ body {
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  padding: 20px;
+}
+
+.menu-content {
+  display: none;
 }
 </style>
