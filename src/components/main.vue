3cf09f479296834b0750acb8ba4d2fb826282af1
JustKooba <jakubsakw2009@gmail.com>
2023-05-27 19:30:46 +0200

    styled the menu

--- src/components/main.vue
+++ src/components/main.vue
@@ -10,11 +10,16 @@
       />
 
       <div class="menu-content" :style="{ display: menuDisplay }">
+        <img
+          src="../assets/shared/icon-close.svg"
+          alt="X Icon"
+          @click="menuClick"
+        />
         <ul>
-          <li>00 Home</li>
-          <li>01 Destination</li>
-          <li>02 Crew</li>
-          <li>03 Technology</li>
+          <li><span class="num">00</span> Home</li>
+          <li><span class="num">01</span> Destination</li>
+          <li><span class="num">02</span> Crew</li>
+          <li><span class="num">03</span> Technology</li>
         </ul>
       </div>
     </div>
@@ -152,15 +157,52 @@ body {
 
 .menu-content {
   display: none;
-  color: #a9aab8;
+  color: #fff;
   position: absolute;
-  background-color: #fff;
-  backdrop-filter: blur(10px);
-  opacity: 0.7;
+  background-color: rgba(27, 27, 27, 0.6);
+  -webkit-backdrop-filter: blur(30px);
+  backdrop-filter: blur(30px);
+  height: 100%;
+  top: 0;
+  width: 70%;
+  right: 0.1px;
+  z-index: 1045;
+  font-family: "Barlow Condensed", sans-serif;
+}
+
+.menu-content ul {
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  font-size: 27px;
+  margin-left: 35px;
+  margin-top: 100px;
+}
+
+.menu-content ul li {
+  margin-top: 2rem;
+}
+
+.menu-content ul li:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+.menu-content img {
+  height: 30px;
+  position: absolute;
+  top: 20px;
+  right: 20px;
 }
 
 .menu img {
   cursor: pointer;
+}
+
+.num {
+  font-weight: 700;
+  font-size: 24px;
+  font-family: "Bellefair", sans-serif;
 }
 
 header {
