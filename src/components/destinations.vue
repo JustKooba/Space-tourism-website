6b3b4a3145a90936cd8564c31d5fa517283ba75e
JustKooba <jakubsakw2009@gmail.com>
2023-06-02 17:36:00 +0200

    destinations setup

--- src/components/destinations.vue
+++ src/components/destinations.vue
@@ -1,9 +1,39 @@
 <template>
-  <div></div>
+  <div class="content">
+    <h2><span class="num dest">01</span>Pick your destination</h2>
+    <div class="destination">
+      <img src="" alt="" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import data from "../assets/data.json";
+export default {
+  data() {
+    return {
+      data: data,
+    };
+  },
+};
 </script>
 
-<style></style>
+<style>
+.content {
+  width: 100vw;
+}
+h2 {
+  color: #fff;
+  font-family: "Bellefair", sans-serif;
+  text-transform: uppercase;
+  text-align: center;
+  font-size: 19px;
+}
+.num {
+  color: #707070;
+}
+
+.dest {
+  font-size: 19px;
+}
+</style>
