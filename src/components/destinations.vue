b1e54c1f491929fc19bc019fa4508355c3c99ae5
JustKooba <jakubsakw2009@gmail.com>
2023-06-05 21:32:31 +0200

    Importing images from a json file

--- src/components/destinations.vue
+++ src/components/destinations.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="content">
     <h2><span class="num dest">01</span>Pick your destination</h2>
-    <div class="destination">
-      <img src="" alt="" />
+    <div class="destination" v-if="destinations.length > 0">
+      <img :src="getImagePath(destinations[0].images.webp)" alt="Image" />
     </div>
   </div>
 </template>
 
 <script>
 import data from "../assets/data.json";
+
 export default {
   data() {
     return {
-      data: data,
+      destinations: [],
     };
+  },
+  mounted() {
+    this.destinations = data.destinations;
+  },
+  methods: {
+    getImagePath(image) {
+      return require(`../assets/destination/${image}`);
+    },
   },
 };
 </script>
-
 <style>
 .content {
   width: 100vw;
