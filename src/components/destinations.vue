61dc6d665b5252e4eb6ce509df951e5b2332bb3e
JustKooba <jakubsakw2009@gmail.com>
2023-06-06 22:02:46 +0200

    added destination buttons

--- src/components/destinations.vue
+++ src/components/destinations.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="content">
-    <h2><span class="num dest">01</span>Pick your destination</h2>
+    <h2 class="pick"><span class="num dest">01</span>Pick your destination</h2>
     <div class="destination" v-if="destinations.length > 0">
       <img :src="getImagePath(destinations[0].images.png)" alt="Image" />
+    </div>
+    <div class="buttons">
+      <button class="dest-btn">Moon</button>
+      <button class="dest-btn">Mars</button>
+      <button class="dest-btn">Europa</button>
+      <button class="dest-btn">Titan</button>
     </div>
   </div>
 </template>
@@ -44,5 +50,38 @@ h2 {
 
 .dest {
   font-size: 19px;
+}
+
+.pick {
+  margin-bottom: 25px;
+}
+
+.destination img {
+  height: 180px;
+}
+
+.dest-btn {
+  background: transparent;
+  border: none;
+  font-family: "Barlow Condensed", sans-serif;
+  font-size: 20px;
+  font-weight: 700;
+  text-transform: uppercase;
+  color: #fff;
+  display: flex;
+}
+
+.buttons {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+}
+
+.buttons button:hover {
+  text-decoration: underline;
+  cursor: pointer;
 }
 </style>
